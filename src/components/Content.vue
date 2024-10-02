e296fed3827bba8dafc720e9892fd7a906d83746
Arthur Soares <arthursoares0217@gmail.com>
2024-10-02 11:34:58 -0300

    Fix image paths

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -9,7 +9,7 @@
       subtitle: 'Casa de cúpula em Salesópolis, Brasil',
       rating: '4,98 (123)',
       price: 'R$723',
-      src: 'src/assets/images/image-1.webp'
+      src: '/assets/images/image-1.webp'
     },
     {
       id: 2,
@@ -17,7 +17,7 @@
       subtitle: 'Cabana em Paraty, Brasil',
       rating: '4,96 (26)',
       price: 'R$639',
-      src: 'src/assets/images/image-2.webp'
+      src: '/assets/images/image-2.webp'
     },
     {
       id: 3,
@@ -25,7 +25,7 @@
       subtitle: 'Casa de cúpula em Campos do Jordão...',
       rating: '5,0 (113)',
       price: 'R$685',
-      src: 'src/assets/images/image-3.webp'
+      src: '/assets/images/image-3.webp'
     },
     {
       id: 4,
@@ -33,7 +33,7 @@
       subtitle: 'Chalé em Campos do Jordão, Brasil',
       rating: '5,0 (65)',
       price: 'R$868',
-      src: 'src/assets/images/image-4.webp'
+      src: '/assets/images/image-4.webp'
     },
     {
       id: 5,
@@ -41,7 +41,7 @@
       subtitle: 'Microcasa em Ibiúna, Brasil',
       rating: '4.98 (81)',
       price: 'R$1.480',
-      src: 'src/assets/images/image-5.webp'
+      src: '/assets/images/image-5.webp'
     },
   ])
 
@@ -52,7 +52,7 @@
       subtitle: 'Cabana em Ubatuba, Brasil',
       rating: '4,94 (148)',
       price: 'R$1.200',
-      src: 'src/assets/images/image-6.webp'
+      src: '/assets/images/image-6.webp'
     },
     {
       id: 2,
@@ -60,7 +60,7 @@
       subtitle: 'chalé em Brasil',
       rating: '4,97 (178)',
       price: 'R$595',
-      src: 'src/assets/images/image-7.webp'
+      src: '/assets/images/image-7.webp'
     },
     {
       id: 3,
@@ -68,7 +68,7 @@
       subtitle: 'Apartamento em São Paulo, Brasil',
       rating: '4,93 (122)',
       price: 'R$331',
-      src: 'src/assets/images/image-8.webp'
+      src: '/assets/images/image-8.webp'
     },
     {
       id: 4,
@@ -76,7 +76,7 @@
       subtitle: 'Chalé em Ilhabela, Brasil',
       rating: '4,99 (162)',
       price: 'R$594',
-      src: 'src/assets/images/image-9.webp'
+      src: '/assets/images/image-9.webp'
     },
     {
       id: 5,
@@ -84,7 +84,7 @@
       subtitle: 'Casa de hóspedes em Mairiporã, Brasil',
       rating: '4.94 (66)',
       price: 'R$232',
-      src: 'src/assets/images/image-10.webp'
+      src: '/assets/images/image-10.webp'
     },
   ])
 </script>
