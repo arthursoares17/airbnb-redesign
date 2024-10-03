940c3307d2f563d7f236196de4973c13af8bf091
Arthur Soares <arthursoares0217@gmail.com>
2024-10-02 23:40:20 -0300

    add header mobile

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,27 @@
 <script setup>
+  import { onMounted, onUnmounted, ref } from 'vue'
   import User from '@/components/User'
+
+  const windowWidth = ref(null)
+  const isHidden = ref(false)
+  const mobileNav = ref(false)
+
+  function mobileWidth() {
+    windowWidth.value = window.innerWidth
+    if(windowWidth.value <= 768) {
+      isHidden.value = true
+      mobileNav.value = true
+      return
+    }
+  }
+
+  onMounted(() => {
+    window.addEventListener('resize', mobileWidth)
+  })
+
+  onUnmounted(() => {
+    window.removeEventListener('resize', mobileWidth)
+  })
 </script>
 
 <template>
@@ -11,7 +33,10 @@
       />
     </div>
 
-    <nav class="header__navigation">
+    <nav 
+      v-bind:class="{ 'header__navigation--hidden': isHidden }"
+      class="header__navigation"
+    >
       <ul class="header__list">
         <span class="header__item header__item--active">
           Acomodações
@@ -28,6 +53,28 @@
     </nav>
 
     <User/>
+
+    <nav 
+      v-if="mobileNav"
+      class="navigation__mobile"
+    >
+      <ul class="navigation__list">
+        <li class="navigation__item navigation__item--active">
+          <i class="bi bi-house-door"></i>
+          Início
+        </li>
+
+        <li class="navigation__item">
+          <i class="bi bi-search"></i>
+          Explorar
+        </li>
+
+        <li class="navigation__item">
+          <i class="bi bi-heart"></i>
+          Favoritos
+        </li>
+      </ul>
+    </nav>
   </header>
 </template>
 
